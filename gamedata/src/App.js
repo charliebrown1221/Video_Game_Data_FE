a2d68d50bb3111d45a2968451b105390f08e2508
charliebrown1221 <chaimbrown1@yahoo.com>
2022-07-07 12:58:01 -0400

    created axios.get + components chartdata, searchbar and datamapper(might not use)

--- gamedata/src/App.js
+++ gamedata/src/App.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React,{useState} from 'react';
+import axios from 'axios';
+import SearchBar from './components/SearchBar'
+import ChartData from './components/ChartData'
+
+
+
 
 function App() {
 
+  const [allData, setAllData]=useState([]);
+
+  async function getAllData(){
+    let response =await axios.get('http://localhost:8080/all/')
+    setAllData(response.data)
+    console.log(response.data)
+  }
 
 
 
 
 
-  
   return (
-    <div className="App">
-      <h1>hello world</h1>
+    <div>
+    <div>
+      <SearchBar />
+    </div>
+    <div><ChartData />
+    </div>
     </div>
   );
 }
